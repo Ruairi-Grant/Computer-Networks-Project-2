057ef400d4dcc4b43df31b3edf739693c641b555
Ruairi-Grant <grantru@tcd.ie>
2022-04-07 00:06:55 +0100

    Bus takes user input

--- bus.py
+++ bus.py
@@ -1,18 +1,44 @@
 import socket
 import time
+import sys
 
 eta = 120
 
-serverAddressPort = ("127.0.0.1", 65432)
+if len(sys.argv) < 4:
+    print(f"Usage: {sys.argv[0]} <start_node> <node_list[0]> <node_list[1]> ...")
+    sys.exit(1)
+
+node_id = sys.argv[1]
+print(f"RSU {node_id} client process active.")
+
+print(f"Node list read in: {sys.argv[2]}, {sys.argv[3]}")
+
+if sys.argv[2] == "A":
+    host = "127.0.0.1"
+    port = 33500    
+if sys.argv[2] == "B":
+    host = "127.0.0.2"
+    port = 33500
+if sys.argv[2] == "C":
+    host = "127.0.0.3"
+    port = "33500"    
+if sys.argv[2] == "D":
+    host = "127.0.0.4"
+    port = "33500"  
+if sys.argv[2] == "E":
+    host = "127.0.0.5"
+    port = "33500"
+
+serverAddressPort = (host, port)
 buffer = 1024
 i = 0
 finished = False
 
-# Path planning algorithm will be implemented here to generate a list of nodes along the EV's route
+# Path planning algorithm will be implemented here to generate a list of nodes along the Bus's route
 def generate_node_list():
     return ("A B C /")
 
-# EV acquires the address and port of the next RSU in its node list from a database
+# Bus acquires the address and port of the next RSU in its node list from a database
 def node_ip_address(node):
     return ("127.0.0.1", 65432)
 
@@ -27,7 +53,7 @@ UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 node_list = generate_node_list()
 
 # Search for the IP address of the node we want to contact
-serverAddressPort = node_ip_address(node_list[0])
+# serverAddressPort = node_ip_address(node_list[0])
 
 # Send to server using created UDP socket
 while not finished:
